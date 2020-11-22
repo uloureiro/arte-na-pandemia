4b1d5ea1595249a288f309982e6ee0e6df1c735c
uloureiro <urick@urick.com.br>
2020-11-22 14:16:30 -0300

    Adiciona pré renderização

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,27 @@
+const PrerenderSpaPlugin = require('prerender-spa-plugin')
+const path = require('path')
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.resolve(__dirname, 'dist'),
+    routes: ['/'],
+    postProcess (context) {
+      const title = 'Arte na Pandemia'
+      context.html = context.html.replace(
+        /<title>[^<]*<\/title>/i,
+        `<title>${title}</title>`
+      )
+      return context
+    }
+  })
+]
+
 module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(...productionPlugins)
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
